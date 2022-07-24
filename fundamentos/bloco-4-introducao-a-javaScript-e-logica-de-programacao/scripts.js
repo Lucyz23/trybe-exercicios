6a4b8a361eeabf585fdbd3fd4d0d3e059fc56ada
Lucyz23 <lucymagalhaesbarbosa@gmail.com>
2022-07-23 22:37:04 -0300

    Resolvendo alguns dos 15 projetos

--- fundamentos/bloco-4-introducao-a-javaScript-e-logica-de-programacao/scripts.js
+++ fundamentos/bloco-4-introducao-a-javaScript-e-logica-de-programacao/scripts.js
@@ -1,8 +1,51 @@
-let conteNumeros = 9;
-for (let contador = 2; contador < 150; contador++){
-    if(contador % 3 === 0) {
-        conteNumeros = conteNumeros++;
-    }
-} if(conteNumeros === 50){
-    console.log('Mensagem secreta!');
+var A = 34;
+var B = 40;
+console.log(A + B);
+
+var A = 34;
+var B = 40;
+console.log(A - B);
+
+var A = 34;
+var B = 40;
+console.log(A / B);
+
+var A = 34;
+var B = 40;
+console.log(A % B);
+
+const a = 5;
+const b = 10;
+const c = 6;
+
+if (a > b && a > c){
+    console.log( 'numero maior é A');
+} else {
+    console.log ('numero maior é C');
+} if (b > c){
+    console.log('Maior é b');
+    
+} else {
+    console.log ('Maior é C');
+}
+
+const valorIndefinido =-1;
+
+if(valorIndefinido >= 1){
+    console.log('Positive');
+} else if (
+    valorIndefinido === 0
+) {console.log('Zero');
+} else {console.log ('Negative')};
+
+const x = 10;
+const z= 40;
+const y= 80;
+
+if ( x+y+z===180 ) {
+    return true;
+} else {
+    return false;
+} if (x < 0 || z < 0 || y < 0) {
+    return console.error ('Angulo definido errado');
 }
