bedcedc9bfa58d86bef944e76eb92e313af94b62
Lucyz23 <lucymagalhaesbarbosa@gmail.com>
2022-08-25 12:01:08 -0300

    Exemplo1

---
+++ fundamentos/bloco-08-higher-order-functions-do-javascript-es6/dia-5-javascript-es6-spread-operator-parametro-rest-destructuring-e-mais/teste1.js
@@ -0,0 +1,23 @@
+// Faça uma lista com as suas frutas favoritas
+const specialFruit = ['melancia', 'morango', 'mamão'];
+
+// Faça uma lista de complementos que você gostaria de adicionar
+const additionalItens = ['pêra', 'leite-condensado', 'melão'];
+
+const fruitSalad = (fruit, additional) => {
+  const salad = [...fruit, ...additional];
+  return salad;
+};
+
+console.log(fruitSalad(specialFruit, additionalItens));
+
+//exemplo2: 
+
+const specialPeople = ['José', 'Joana', 'Megas', 'Renato'];
+const specialPeople2 = ['Rafa', 'Luna', 'Aysla'];
+
+const allPeople = (people, people2) => {
+    const conjuntoPeople = [...people, ...people2];
+    return conjuntoPeople;
+}
+console.log(allPeople(specialPeople, specialPeople2));
